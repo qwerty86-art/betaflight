30d0c8a045113678dcc739c29bcaae14e5f68f44
qwerty86-art <kramarenko870@ukr.net>
2026-08-04 15:59:56 +0300

    Comment out ARMED flag checks in VTX processing
    Comment out the ARMING_FLAG check in vtxProcessBandAndChannel and vtxProcessFrequency functions.

--- src/main/io/vtx.c
+++ src/main/io/vtx.c
@@ -139,7 +139,7 @@ STATIC_UNIT_TESTED vtxSettingsConfig_t vtxGetSettings(void)
 
 static bool vtxProcessBandAndChannel(vtxDevice_t *vtxDevice)
 {
-    if (!ARMING_FLAG(ARMED)) {
+//    if (!ARMING_FLAG(ARMED)) {
         uint8_t vtxBand;
         uint8_t vtxChan;
         if (vtxCommonGetBandAndChannel(vtxDevice, &vtxBand, &vtxChan)) {
@@ -149,14 +149,14 @@ static bool vtxProcessBandAndChannel(vtxDevice_t *vtxDevice)
                 return true;
             }
         }
-    }
+//    }
     return false;
 }
 
 #if defined(VTX_SETTINGS_FREQCMD)
 static bool vtxProcessFrequency(vtxDevice_t *vtxDevice)
 {
-    if (!ARMING_FLAG(ARMED)) {
+//    if (!ARMING_FLAG(ARMED)) {
         uint16_t vtxFreq;
         if (vtxCommonGetFrequency(vtxDevice, &vtxFreq)) {
             const vtxSettingsConfig_t settings = vtxGetSettings();
@@ -165,7 +165,7 @@ static bool vtxProcessFrequency(vtxDevice_t *vtxDevice)
                 return true;
             }
         }
-    }
+//    }
     return false;
 }
 #endif
